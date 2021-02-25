0efd12b3d5e2222728963f77e81b2b3ddc0263fa
seryozhabaleyko <seryozha.baleyko@gmail.com>
2021-02-25 10:41:06 +0300

    Fix localization

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { CustomRouteConfig } from '@tager/admin-layout';
 
 import MenuEditorView from './views/MenuEditor';
 import { MENU_ROUTE_PATHS } from './constants/paths';
+import { i18n } from '@tager/admin-services';
+import EN from './locales/en';
+import RU from './locales/ru';
+
+i18n.addTranslations('en', 'menus', EN);
+i18n.addTranslations('ru', 'menus', RU);
 
 export * from './constants/routes';
 export * from './constants/paths';
