ae30edbf2b4ffca280ca0dcff408095dd1d2cce3
Vital Ozierski <vital@ozerich.com>
2022-12-19 14:45:34 +0300

    1.0.5 - simplify getMenuPageUrl function

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -2,6 +2,8 @@ import { compile } from "path-to-regexp";
 
 import { MENU_ROUTE_PATHS } from "../constants/paths";
 
-export function getMenuPageUrl(params: { menuAlias: string }): string {
-  return compile(MENU_ROUTE_PATHS.MENU_PAGE)(params);
+export function getMenuPageUrl(menuAlias: string): string {
+  return compile(MENU_ROUTE_PATHS.MENU_PAGE)({
+    menuAlias
+  });
 }
